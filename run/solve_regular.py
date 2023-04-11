e5c70d41986438f17d45c94a93aa3b2d0901b00f
LaptopHeaven <courtney@kygeek.com>
2023-04-11 15:02:46 -0400

    updated settings for live pull

--- run/solve_regular.py
+++ run/solve_regular.py
@@ -1,18 +1,25 @@
-import os
-import sys
-import pathlib
-import json
-import datetime
-import pandas as pd
 import argparse
+import datetime
+import json
+import os
+import pathlib
+import sys
 
-if __name__=="__main__":
+import pandas as pd
+
+if __name__ == "__main__":
     base_folder = pathlib.Path()
     sys.path.append(str(base_folder / "../src"))
-    from multi_period_dev import connect, get_my_data, prep_data, solve_multi_period_fpl, generate_team_json
-    import data_parser as pr
 
-    with open('../data/regular_settings.json') as f:
+    from multi_period_dev import (
+        connect,
+        generate_team_json,
+        get_my_data,
+        prep_data,
+        solve_multi_period_fpl,
+    )
+
+    with open("../data/regular_settings.json") as f:
         options = json.load(f)
 
         parser = argparse.ArgumentParser(add_help=False)
@@ -26,14 +33,20 @@ if __name__=="__main__":
         options = {**options, **vars(args)}
 
     if options.get("cbc_path") != "":
-        os.environ['PATH'] += os.pathsep + options.get("cbc_path")
+        os.environ["PATH"] += os.pathsep + options.get("cbc_path")
 
     if options.get("preseason"):
-        my_data = {'picks': [], 'chips': [], 'transfers': {'limit': None, 'cost': 4, 'bank': 1000, 'value': 0}}
+        my_data = {
+            "picks": [],
+            "chips": [],
+            "transfers": {"limit": None, "cost": 4, "bank": 1000, "value": 0},
+        }
     elif options.get("use_login", False):
         session, team_id = connect()
         if session is None and team_id is None:
             exit(0)
+        else:
+            my_data = get_my_data(session, team_id)
     else:
         if options.get("team_data", "json").lower() == "id":
             team_id = options.get("team_id", None)
@@ -43,7 +56,7 @@ if __name__=="__main__":
             my_data = generate_team_json(team_id)
         else:
             try:
-                with open('../data/team.json') as f:
+                with open("../data/team.json") as f:
                     my_data = json.load(f)
             except FileNotFoundError:
                 print(
@@ -51,19 +64,20 @@ if __name__=="__main__":
                         1. Download your team data from https://fantasy.premierleague.com/api/my-team/YOUR-TEAM-ID/ and
                             save it under data folder with name 'team.json', or
                         2. Set "team_data" in regular_settings to "ID", and set the "team_id" value to your team's ID
-                    """)
+                    """
+                )
                 exit(0)
     data = prep_data(my_data, options)
 
     response = solve_multi_period_fpl(data, options)
     for result in response:
-        iter = result['iter']
-        print(result['summary'])
+        iter = result["iter"]
+        print(result["summary"])
         time_now = datetime.datetime.now()
         stamp = time_now.strftime("%Y-%m-%d_%H-%M-%S")
         if not (os.path.exists("../data/results/")):
             os.mkdir("../data/results/")
-        result['picks'].to_csv(f"../data/results/regular_{stamp}_{iter}.csv")
+        result["picks"].to_csv(f"../data/results/regular_{stamp}_{iter}.csv")
 
     result_table = pd.DataFrame(response)
-    print(result_table[['iter', 'sell', 'buy', 'score']])
+    print(result_table[["iter", "sell", "buy", "score"]])
